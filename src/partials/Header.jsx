75cb5bdf02ff25219b71479a388dec535b75e0be
HannanAmar05 <112670911+HannanAmar05@users.noreply.github.com>
2023-10-06 23:51:59 +0700

    test update

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -8,11 +8,12 @@ import  Button  from '../components/Button'
 export const Header = () => {
   return (
     <>
+    <div></div>
     <div className='px-20 mt-4 mx-auto flex items-center justify-between fixed top-0 left-0 z-50 w-full'>
       <Logo></Logo>
       <Search></Search>
       <div className='flex'>
-      <Button variant='Login'>Login</Button>
+      <Button variant='Login'>TESTING</Button>
       <Button variant='Register'>Register</Button>
       </div>
     </div>
